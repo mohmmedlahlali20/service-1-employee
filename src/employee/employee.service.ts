be58806ea4c6a191957c3429813d55b2d84a512d
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-19 09:17:06 +0100

    feat: amélioration des méthodes dans le service des employés

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Employee } from './schemas/employee.schema';
 import { Model } from 'mongoose';
+import { Employee } from './schema/employe.schema';
 
 @Injectable()
 export class EmployeeService {
   constructor(
-    @InjectModel(Employee.name) private readonly employeeModel: Model<Employee>,
+    @InjectModel(Employee.name) private readonly employeModel: Model<Employee>,
   ) {}
+  
+  importEmployees(path: any) {
+    throw new Error('Method not implemented.');
+  }
+
 
   async create(createEmployeeDto: CreateEmployeeDto): Promise<Employee> {
-    const newEmployee = new this.employeeModel(createEmployeeDto);
+    const newEmployee = new this.employeModel(createEmployeeDto);
     return await newEmployee.save();
   }
 
   async findAll(): Promise<Employee[]> {
-    return await this.employeeModel.find().exec();
+    return await this.employeModel.find().exec();
   }
 
   async findOne(id: string): Promise<Employee> {
-    return await this.employeeModel.findById(id).exec();
+    return await this.employeModel.findById(id).exec();
   }
 
   async update(id: string, updateData: Partial<Employee>): Promise<Employee> {
-    return await this.employeeModel.findByIdAndUpdate(id, updateData, {
+    return await this.employeModel.findByIdAndUpdate(id, updateData, {
       new: true,
     }).exec();
   }
