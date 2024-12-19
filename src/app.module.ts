23fab09929644aae4626fc8961265fd93f004981
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-12-19 09:16:56 +0100

    refactor: mise à jour de l'importation des modules dans app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmployeeModule } from './employee/employee.module';
+import { EmployeModule } from './employee/employee.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/candidatures'),
-    EmployeeModule
+    EmployeModule
   ],
   controllers: [AppController],
   providers: [AppService],
